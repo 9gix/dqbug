d413deb68d5f1b7d1fe7667aad76a3fc41c6097a
Eugene <yeo.eugene.oey@gmail.com>
2012-07-21 03:24:50 +0800

    Add South Migration

--- dqbug/settings.py
+++ dqbug/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
